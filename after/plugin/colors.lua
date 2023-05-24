370bab95f208e7f01434a21206119f040e029713
happsiexd <njesper98@gmail.com>
2023-05-24 21:49:08 +0200

    update configs

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,9 +1,16 @@
+--[[
 function ColorMyPencils(color)
 	color = color or "rose-pine"
 	vim.cmd.colorscheme(color)
-	
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorMyPencils()
+]]--
+
+vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
+
+require("catppuccin").setup()
+
+vim.cmd [[colorscheme catppuccin]]
